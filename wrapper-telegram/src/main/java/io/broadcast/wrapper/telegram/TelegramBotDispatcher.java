7c90bcff1791fbc737e8c3993435c751722cc5a5
xxcoldinme <misha_stonlex@bk.ru>
2024-12-16 11:40:04 +0300

    fix telegram-wrapper announcements subject serialize

--- wrapper-telegram/src/main/java/io/broadcast/wrapper/telegram/TelegramBotDispatcher.java
+++ wrapper-telegram/src/main/java/io/broadcast/wrapper/telegram/TelegramBotDispatcher.java
@@ -1,12 +1,15 @@
 package io.broadcast.wrapper.telegram;
 
 import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.model.MessageEntity;
 import com.pengrad.telegrambot.request.SendMessage;
 import com.pengrad.telegrambot.response.SendResponse;
 import io.broadcast.engine.Announcement;
 import io.broadcast.engine.TextMessage;
 import io.broadcast.engine.dispatch.BroadcastDispatcher;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.Optional;
 
 public class TelegramBotDispatcher<T> implements BroadcastDispatcher<Long, T> {
 
@@ -26,10 +29,15 @@ public class TelegramBotDispatcher<T> implements BroadcastDispatcher<Long, T> {
 
     @Override
     public void dispatch(@NotNull Announcement<Long, T> announcement) {
-        TextMessage textMessage = announcement.getTextMessage();
-        SendResponse sendResponse = telegramBot.execute(new SendMessage(
+        SendMessage messageRequest = createSendMessageRequest(
                 announcement.getRecord().getId(),
-                textMessage.getSubject()));
+                announcement.getTextMessage());
+
+        if (messageRequest == null) {
+            return;
+        }
+
+        SendResponse sendResponse = telegramBot.execute(messageRequest);
 
         if (!sendResponse.isOk()) {
             int errorCode = sendResponse.errorCode();
@@ -37,5 +45,24 @@ public class TelegramBotDispatcher<T> implements BroadcastDispatcher<Long, T> {
 
             throw new BroadcastTelegramBotException("Failed dispatch message to telegram bot [errorCode=" + errorCode + ", description=" + description + "]");
         }
+    }
+
+    private SendMessage createSendMessageRequest(long chatID, TextMessage textMessage) {
+        if (textMessage == null || textMessage.getContent() == null) {
+            return null;
+        }
+
+        String subjectString = Optional.of(textMessage).map(TextMessage::getSubject).orElse("");
+        String contentString = textMessage.getContent();
+
+        String messageString = (subjectString + "\n" + contentString);
+
+        SendMessage sendMessage = new SendMessage(chatID, messageString);
+
+        if (textMessage.hasSubject()) {
+            sendMessage = sendMessage.entities(
+                    new MessageEntity(MessageEntity.Type.bold, 0, textMessage.getSubject().length()));
+        }
+        return sendMessage;
     }
 }
