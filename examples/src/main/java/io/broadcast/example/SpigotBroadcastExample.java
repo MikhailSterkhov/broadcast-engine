8f839d451e9707bfa0554d62994b0271b7a41813
xxcoldinme <misha_stonlex@bk.ru>
2024-12-20 17:15:24 +0300

    added `wrapper-spigot` module and spigot-impls

--- examples/src/main/java/io/broadcast/example/SpigotBroadcastExample.java
+++ examples/src/main/java/io/broadcast/example/SpigotBroadcastExample.java
@@ -2,15 +2,9 @@ package io.broadcast.example;
 
 import io.broadcast.engine.BroadcastEngine;
 import io.broadcast.engine.announcement.AnnouncementExtractor;
-import io.broadcast.engine.announcement.StringAnnouncement;
-import io.broadcast.wrapper.jeds.JedisBroadcastPipeline;
-import io.broadcast.wrapper.jeds.dispatcher.JedisDispatcher;
-import io.broadcast.wrapper.jeds.extractor.JedisRecordExtractor;
 import io.broadcast.wrapper.spigot.SpigotBroadcastPipeline;
 import io.broadcast.wrapper.spigot.extractor.announcement.PermissibleStringAnnouncement;
 import io.broadcast.wrapper.spigot.extractor.announcement.SpigotAnnouncementExtractor;
-import org.bukkit.entity.Player;
-import redis.clients.jedis.Jedis;
 
 public class SpigotBroadcastExample {
 
